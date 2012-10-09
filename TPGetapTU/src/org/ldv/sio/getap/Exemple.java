78adc33d567958a52f07de469037bb2a374930db
nbarroso <nbarroso@btssio.vinci-melun.org>
2012-10-09 11:17:08 +0200

    Passage de la dvctap par les différents états.

--- TPGetapTU/src/org/ldv/sio/getap/Exemple.java
+++ TPGetapTU/src/org/ldv/sio/getap/Exemple.java
@@ -20,6 +20,29 @@ public class Exemple {
 		dvctap = new DemandeValidationConsoTempsAccPers(0l, "2012-2013", date, 240, prof, accPers, eleve, 0);
 		
 		System.out.println(dvctap.toString());
+		
+		
+		dvctap.setEtat(0);
+		System.out.println("Demande crée par l'élève : "+dvctap.getEtat());
+		
+		dvctap.setEtat(4);
+		System.out.println("Demande modifiée par l'élève : "+dvctap.getEtat());
+		
+		dvctap.setEtat(32);
+		System.out.println("Demande validée par le professeur : "+dvctap.getEtat());
+		
+		dvctap.setEtat(4);
+		System.out.println("Demande modifiée par l'élève : "+dvctap.getEtat());
+		
+		dvctap.setEtat(2048);
+		System.out.println("Durée modifiée par le professeur : "+dvctap.getEtat());
+		
+		dvctap.setEtat(2);
+		System.out.println("Demande rejetée par l'élève : "+dvctap.getEtat());
+		
+		dvctap.setEtat(32);
+		System.out.println("Demande validée par le professeur : "+dvctap.getEtat());
+		
 	}
 
 }
